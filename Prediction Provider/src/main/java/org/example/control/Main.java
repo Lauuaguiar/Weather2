cbec573b3498b4d9dcce27da8269e03513eb8ec6
Lauea <laura.aguiar101@alu.ulpgc.es>
2023-12-04 19:11:44 +0000

    Creation of the Prediction Provider module and addition of the code to it. Creation of the Event Store Builder module

--- Prediction Provider/src/main/java/org/example/control/Main.java
+++ Prediction Provider/src/main/java/org/example/control/Main.java
@@ -6,7 +6,7 @@ import java.util.TimerTask;
 public class Main {
     public static void main(String[] args) {
         try {
-            String locations = "src/main/resources/locations.csv";
+            String locations = "Prediction Provider\\src\\main\\resources\\locations.csv";
             WeatherController weatherController = new WeatherController(new OpenWeatherMapSupplier(args[0]), new SqliteWeatherStore(), new WeatherDataPublisher());
 
             Timer timer = new Timer();
